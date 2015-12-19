859ce097668c44656667fc8ccf3a585256d9aee9
Joslyn <sallpally@gmail.com>
2015-12-18 18:56:17 -0800

    Hogger stuff

---
+++ sql/updates/2015_11_18_01.sql
@@ -0,0 +1,15 @@
+-- Hogger stuff
+UPDATE `creature_template` 
+	SET `ScriptName` = 'npc_hogger'  WHERE `entry` = '448' ;
+
+
+
+DELETE FROM `creature` WHERE `guid` = '167936' ;
+
+DELETE FROM `creature` WHERE `guid` = '167939' ;
+
+DELETE FROM `creature` WHERE `guid` = '167937' ;
+
+DELETE FROM `creature` WHERE `guid` = '167938' ;
+
+DELETE FROM `creature` WHERE `guid` = '167940' ;
